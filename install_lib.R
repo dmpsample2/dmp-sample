9b74686da42d5fdeac535bcfa7855a2544fdfe6d
ryu.nakanishi <ryu.nakanishi@brainpad.co.jp>
2014-09-29 16:26:04 +0900

    R-Libraryインストールファイル新規追加

---
+++ install_lib.R
@@ -0,0 +1,8 @@
+options(repos="http://cran.md.tsukuba.ac.jp")
+
+install.packages("ggplot2")
+install.packages("knitr")
+install.packages("hexbin")
+install.packages("quantreg")
+install.packages("ROCR")
+install.packages("rms")
